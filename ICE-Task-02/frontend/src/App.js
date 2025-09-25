a77751d7e0d70536b9dd4ee9b0e30f0ac2839adf
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-09-25 21:20:44 +0200

    feat(2): update default homepage text in app

--- ICE-Task-02/frontend/src/App.js
+++ ICE-Task-02/frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to APDS
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          We are using react for the frontend
         </a>
       </header>
     </div>
