349f4873ccfa551879aaaa82c64eaed7009a645f
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-09-25 16:18:07 +0200

    feat(1): add initial server on localhost loopback port

--- ICE-Task-01/BACKEND/server.js
+++ ICE-Task-01/BACKEND/server.js
@@ -1,1 +1,8 @@
-console.log('APDS makes me cry!')
+import http from 'http';
+
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+    res.end('I am crying less because my server is working')
+})
+
+server.listen(PORT)
