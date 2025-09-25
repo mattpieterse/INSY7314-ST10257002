efcb3c126f74d49fe770d4d1bbe3fea05657417d
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-09-25 20:40:37 +0200

    feat(2): add more http operations to the post endpoint

--- ICE-Task-02/BACKEND/routes/post.mjs
+++ ICE-Task-02/BACKEND/routes/post.mjs
@@ -24,4 +24,43 @@ router.post('/', async (req, res) => {
     res.status(201).send(result);
 });
 
+// Update a record by id.
+router.patch('/:id', async (req, res) => {
+    const query = { _id: new ObjectId(req.params.id) };
+    const updateDocument = {
+        $set: {
+            user: req.body.user,
+            content: req.body.content,
+            image: req.body.image,
+        },
+    };
+
+    let collection = db.collection('posts');
+    let result = await collection.updateOne(query, updateDocument);
+    res.status(200).send(result);
+});
+
+// Get a single record by id.
+router.get('/:id', async (req, res) => {
+    let collection = db.collection('posts');
+    let query = { _id: new ObjectId(req.params.id) };
+    let result = await collection.findOne(query);
+    
+    if (!result) {
+        return res.status(404).send({ message: 'Post not found' });
+    } else {
+        return res.status(200).send(result);
+    }
+});
+
+// Delete a record by id.
+router.delete('/:id', async (req, res) => {
+    let query = { _id: new ObjectId(req.params.id) };
+
+    let collection = db.collection('posts');
+    let result = await collection.deleteOne(query);
+
+    res.status(200).send(result);
+});
+
 export default router;
