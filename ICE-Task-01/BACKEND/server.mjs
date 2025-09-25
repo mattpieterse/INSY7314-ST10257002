50d2af898318567bade609a07ad1a1e8b9bc0607
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-09-25 16:49:11 +0200

    feat(1): update backend server to export the application and integrate the ssl pem keys

--- ICE-Task-01/BACKEND/server.mjs
+++ ICE-Task-01/BACKEND/server.mjs
@@ -1,37 +1,11 @@
-import express from 'express';
+import http from 'http';
+import fs from 'fs';
+import app from './app.mjs';
 
 const PORT = 3000;
+const server = http.createServer({
+    key: fs.readFileSync('keys/privatekey.pem'),
+    cert: fs.readFileSync('keys/certificate.pem')
+}, app);
 
-const app = express();
-const urlprefix = '/api';
-
-app.use(express.json());
-app.get(urlprefix+'/', (req, res) => {
-    res.send('I am finally figuring this out, no more crying')
-})
-
-app.get(urlprefix+'/orders', (req, res) => {
-    const orders = [
-        {
-            id: "1",
-            name: "Orange"
-        },
-        {
-            id: "2",
-            name: "Apple"
-        },
-        {
-            id: "3",
-            name: "Pear"
-        },
-    ]
-
-    res.json(
-        {
-            message: "Fruits",
-            orders: orders
-        }
-    )
-})
-
-app.listen(PORT)
+server.listen(PORT)
