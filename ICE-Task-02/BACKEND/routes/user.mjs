fc6ed15ef934611f9d6173153de4b4a6cc1fb30c
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-09-25 21:02:38 +0200

    feat(2): add sign in endpoint to the user route

--- ICE-Task-02/BACKEND/routes/user.mjs
+++ ICE-Task-02/BACKEND/routes/user.mjs
@@ -24,4 +24,33 @@ router.post('/signup',  async (req, res) => {
     res.status(201).send(result);
 });
 
+// Login
+router.post('/login', bruteforce.prevent, async (req, res) => {
+    const { name, password } = req.body;
+
+    console.log(name, password);
+
+    try {
+        let collection = db.collection('users');
+        let user = await collection.findOne({ name: name });
+
+        if (!user) {
+            return res.status(401).json({ message: 'Authentication failed' });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            res.status(401).json({ message: 'Authentication failed' });
+        }
+        else {
+            const token = jwt.sign({ username:req.body.name, password:req.body.password }, 'RANDOM_TOKEN_SECRET', { expiresIn: '1h' });
+            res.status(200).json({ userId: user._id, token: token });
+            console.log("TOKEN (JWT):", token);
+        }
+    } catch (error) {
+        console.error('Error during login:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
 export default router;
