ddd6d962b2a558f638b33b5d267f596129cda1e8
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-09-25 16:29:34 +0200

    feat(1): update backend server to include the testing endpoint

--- ICE-Task-01/BACKEND/server.mjs
+++ ICE-Task-01/BACKEND/server.mjs
@@ -8,4 +8,8 @@ app.get('/', (req, res) => {
     res.send('I am finally figuring this out, no more crying')
 })
 
+app.get('/test', (req, res) => {
+    res.send('Testing the /test endpoint')
+})
+
 app.listen(PORT)
