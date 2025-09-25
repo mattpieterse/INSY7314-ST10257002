37c25950024a770a4ba8f3d63b0ab40db0193d72
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-09-25 16:34:52 +0200

    feat(1): update backend server to include a json orders endpoint with seeded fruits

--- ICE-Task-01/BACKEND/server.mjs
+++ ICE-Task-01/BACKEND/server.mjs
@@ -1,15 +1,37 @@
 import express from 'express';
 
 const PORT = 3000;
+
 const app = express();
+const urlprefix = '/api';
 
 app.use(express.json());
-app.get('/', (req, res) => {
+app.get(urlprefix+'/', (req, res) => {
     res.send('I am finally figuring this out, no more crying')
 })
 
-app.get('/test', (req, res) => {
-    res.send('Testing the /test endpoint')
+app.get(urlprefix+'/orders', (req, res) => {
+    const orders = [
+        {
+            id: "1",
+            name: "Orange"
+        },
+        {
+            id: "2",
+            name: "Apple"
+        },
+        {
+            id: "3",
+            name: "Pear"
+        },
+    ]
+
+    res.json(
+        {
+            message: "Fruits",
+            orders: orders
+        }
+    )
 })
 
 app.listen(PORT)
